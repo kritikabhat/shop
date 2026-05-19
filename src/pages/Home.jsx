1a2d0d199bc90c8f3a2e922b0343b212cc222a35
kritika <34717469+kritikabhat@users.noreply.github.com>
2026-05-19 02:47:59 -0400

    feat: UI design and effects for  home page finalized

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,70 @@
 /**
- * no method here is needed! static page
  * 
  * @returns landing page
  */
+import { useNavigate } from "react-router";
+import mainImage from '../assets/alghozy-Z-NuwWXtyqg-unsplash.png'
+
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import XIcon from '@mui/icons-material/X';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
 
 const Home = () => {
+  let navigate = useNavigate();
+
   return (
   <>
-    <section>
-      <div>Welcome to shopping website!</div>
-      <div className="welcomeImgDiv">
+    <section className="homeArea">
+      <div className="middleContainer">
+        <header className="welcomeArea">
+          <div className='textDiv'>
+            <h1>Limited Time Offer! Up to 50% OFF!</h1>
+            <p>Don't Wait - Limited Stock at Unbeatable Prices!</p>
+            <button onClick={() => navigate("/shop")} className="shopBtn" 
+                type="button">Shop Now!</button>
+          </div>
+        </header>
+        <div className="mainImgDiv">
+          <img src={mainImage} alt="a man delivering shopping boxes" />
+        </div>
       </div>
+      <footer>
+        <div class="footer">
+          <div class="row">
+            <a href="#"><i><FacebookIcon /></i></a>
+            <a href="#"><i><InstagramIcon /></i></a>
+            <a href="#"><i><XIcon /></i></a>
+            <a href="#"><i><YouTubeIcon /></i></a>
+            <a href="#"><i><WhatsAppIcon /></i></a>
+          </div>
+          <div class="row">
+            <ul>
+              <li><a href="#">Contact us</a></li>
+              <li><a href="#">Our Services</a></li>
+              <li><a href="#">Privacy Policy</a></li>
+              <li><a href="#">Terms & Conditions</a></li>
+              <li><a href="#">Career</a></li>
+            </ul>
+          </div>
+          <div class="row">
+            <ul>
+              <li><a href="https://fakestoreapi.com/">FakeStoreAPI</a></li>
+              <li><a href="https://unsplash.com/illustrations/delivery-person-pushing-a-cart-with-boxes-Z-NuwWXtyqg?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+              Illustration by <a href="https://unsplash.com/@artgho/illustrations?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+              Alghozy</a></a></li>
+              <li><a href="https://mui.com/">Material UI</a></li>
+              <li><a href="https://mui.com/material-ui/material-icons/">Material Icons</a></li>
+            </ul>
+          </div>
+          <div class="row">
+          Footer cites external UI elements used || Footer design by: <a href="https://codepen.io/alvarotrigo">alvarotrigo</a>
+          </div>
+        </div>
+      </footer>
+
     </section>
-    <footer>
-      <a href="https://fakestoreapi.com/" title="fake store api">Shop items dataset by fakestoreapi.com</a>
-      <br />
-      <a href="" title="icons">Icons & fonts by google fonts</a>
-      <br />
-      Material UI
-    </footer>
   </>
   )
 }
